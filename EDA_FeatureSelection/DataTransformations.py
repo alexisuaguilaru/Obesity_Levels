9d25f174e1546edce11bbf1b9e8327a2b1fe94a7
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2024-09-18 18:31:59 -0600

    ADD function to convert categorical values into numerical values ADD transformed dataset with this function

--- EDA_FeatureSelection/DataTransformations.py
+++ EDA_FeatureSelection/DataTransformations.py
@@ -18,3 +18,20 @@ def NumericalCategoricalLabels(Dataframe:pd.DataFrame) -> tuple[list]:
         else:
             CategoricalLabels.append(label)
     return NumericalLabels , CategoricalLabels
+
+def CategoricalIntoNumerical(Dataframe:pd.DataFrame,CategoricalLabel:str,NumericalValues:dict):
+    """
+        Function to convert categorical values 
+        (strings) into numerical values using 
+        a given dict with categorical to 
+        numerical value
+
+        -- Dataframe : pd.DataFrame :: Dataframe 
+        where is applied this conversion
+        -- CategoricalLabel : str :: Dataframe's 
+        label which is applied this conversion
+        -- NumericalValues : dict :: Numerical 
+        values which replace categorical values
+    """
+    NumericalMap = lambda value: NumericalValues[value]
+    Dataframe[CategoricalLabel] = Dataframe[CategoricalLabel].apply(NumericalMap)
